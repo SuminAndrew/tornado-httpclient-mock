30248a09de3cbc65ec6b29b13ad314c09743c397
andrew <an.sumin@hh.ru>
2018-07-10 10:09:07 +0300

    prepare release 0.1.1

--- setup.py
+++ setup.py
@@ -9,10 +9,12 @@ class TestHook(test):
         import nose
         nose.main(argv=['nosetests', 'tests/', '-v', '--logging-clear-handlers'])
 
+with open('README.md') as fh:
+    long_description = fh.read()
 
 setup(
     name='tornado-httpclient-mock',
-    version='0.1.0',
+    version='0.1.1',
     description='A library for mocking requests in Tornado HTTP client',
     url='https://github.com/SuminAndrew/tornado-httpclient-mock',
     author='Andrew Sumin',
@@ -25,10 +27,14 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development :: Testing',
     ],
-    license="http://www.apache.org/licenses/LICENSE-2.0",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     cmdclass={
         'test': TestHook
     },
